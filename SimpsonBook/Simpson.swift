3772799654e0dfbb334a747d7a93256f900b26e7
Şeyda Aydın <seydaydiin0@gmail.com>
2023-08-04 04:03:07 +0300

    Model commit

---
+++ SimpsonBook/Simpson.swift
@@ -0,0 +1,28 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Şeyda Aydın on 8.06.2023.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var imageView : UIImage
+    var name : String
+    var job : String
+    
+    init(imageView: UIImage, nameLabel: String, jobLabel: String) {
+        self.imageView = imageView
+        self.name = nameLabel
+        self.job = jobLabel
+    }
+    
+    
+    
+    
+    
+    
+}
